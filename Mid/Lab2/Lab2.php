8e48d5fc8f37c829d35b5c8381541b17f5db03d9
Raihan-Karim <raihankarim1234@gmail.com>
2025-07-26 23:18:48 +0600

    full code-1

--- Mid/Lab2/Lab2.php
+++ Mid/Lab2/Lab2.php
@@ -1,1 +1,123 @@
-<p> hello world </p>
+<!Doctype html>
+
+<html>
+    <title> Bank Management System </title>
+    
+    <body>
+        <center>
+            <h1 style ="color:blue;"> Bank Management System </h1>
+            <h2 style ="color: blue"> Your Trusted Financial Partner </h2>
+</center>
+
+<h3>Customer Ragistration Form</h3>
+<table>
+   
+ <tr>
+        <td> Full Name: </td>
+        <td><input type="text"> </td>
+</tr>
+
+<tr>
+        <td> Date of Birth: </td>
+        <td><input type="date"> </td>
+</tr>
+
+ <tr>
+        <td> Language Knows </td>
+        <td> <input type = "radio" name = "des">Male
+         <input type = "radio" name = "des">Female
+         <input type = "radio" name = "des">Other
+</td>
+
+</tr>
+
+
+<tr>
+        <td> Martial Status </td>
+        <td> <select>
+                    <option name="Select">--Select--</option>
+                    <option name="Single">Single</option>
+                    <option name="Married">Married</option>
+                    </td>
+</tr>
+
+<tr>
+        <td> Account Type </td>
+        <td> <select>
+                    <option name="Select">--Select--</option>
+                    <option name="Savings">Savings</option>
+                    <option name="Fixed">Fixed</option>
+                    </td>
+</tr>
+
+ <tr>
+        <td> Initial Deposit Amount </td>
+        <td><input type="text;"> </td>
+</tr>
+
+
+<tr>
+        <td> Mobile Number </td>
+        <td><input type="text;"> </td>
+</tr>
+
+
+<tr>
+        <td> Email Address </td>
+        <td><input type="text;"> </td>
+</tr>
+
+
+
+<tr>
+        <td> Address </td>
+        <td><input type="text;"> </td>
+</tr>
+
+
+<tr>
+        <td> Occupation </td>
+        <td><input type="text;"> </td>
+</tr>
+
+<tr>
+        <td> National ID (NID) </td>
+        <td><input type="text;"> </td>
+</tr>
+
+
+
+
+
+     
+
+<tr>
+    <td> Upload ID Proof</td>
+    <td> <input type  = "file"></td>
+</tr>
+<tr>
+    
+    <td> <input type  = "checkbox"> I agree to the terms and conditions</td>
+</tr>
+
+
+<tr>
+    <td> Comment </td>
+    <td> <input type  = "text"></td>
+
+</tr>
+
+<tr>
+    <td> </td>
+    <td> <input type ="button" value = "Register">
+    <input type ="button" value = "Clear">
+</td>
+     
+</tr>
+
+
+
+
+</body>
+
+    </html>
